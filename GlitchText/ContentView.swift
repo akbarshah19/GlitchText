97dbe146ca49e5274e60ac49f2ddca327114b2ed
akbarshah19 <105868459+akbarshah19@users.noreply.github.com>
2024-06-23 13:11:55 +0500

    Initial Commit

--- GlitchText/ContentView.swift
+++ GlitchText/ContentView.swift
@@ -8,12 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var trigger: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                GlitchText(text: "Hello Guys!", trigger: trigger) {
+                   LinearKeyframe(
+                    GlitchFrame(top: -5, center: 0, bottom: 0, shadowOpacity: 0.2),
+                    duration: 0.1
+                   )
+                    LinearKeyframe(
+                        GlitchFrame(top: -5, center: -5, bottom: -5, shadowOpacity: 0.6),
+                     duration: 0.1
+                    )
+                    LinearKeyframe(
+                        GlitchFrame(top: -5, center: -5, bottom: 5, shadowOpacity: 0.8),
+                     duration: 0.1
+                    )
+                    LinearKeyframe(
+                        GlitchFrame(top: 5, center: 5, bottom: 5, shadowOpacity: 0.1),
+                     duration: 0.1
+                    )
+                    LinearKeyframe(
+                        GlitchFrame(top: 5, center: 0 , bottom: 5, shadowOpacity: 0.5),
+                     duration: 0.1
+                    )
+                    LinearKeyframe(
+                        GlitchFrame(),
+                     duration: 0.1
+                    )
+                }
+                .font(.system(size: 60, weight: .semibold))
+                
+                GlitchText(text: "Hello Guys!", trigger: trigger, shadow: .green) {
+                   LinearKeyframe(
+                    GlitchFrame(top: 5, center: 5, bottom: 0, shadowOpacity: 0.4),
+                    duration: 0.1
+                   )
+                    LinearKeyframe(
+                        GlitchFrame(top: 5, center: -5, bottom: -5, shadowOpacity: 0.2),
+                     duration: 0.1
+                    )
+                    LinearKeyframe(
+                        GlitchFrame(top: -5, center: 5, bottom: 5, shadowOpacity: 0.3),
+                     duration: 0.1
+                    )
+                    LinearKeyframe(
+                        GlitchFrame(top: 5, center: -5, bottom: 5, shadowOpacity: 0.6),
+                     duration: 0.1
+                    )
+                    LinearKeyframe(
+                        GlitchFrame(top: 0, center: 5, bottom: 0, shadowOpacity: 0.9),
+                     duration: 0.1
+                    )
+                    LinearKeyframe(
+                        GlitchFrame(),
+                     duration: 0.1
+                    )
+                }
+                .font(.system(size: 60, weight: .semibold))
+            }
+            
+            Button {
+                trigger.toggle()
+            } label: {
+                Text("Trigger")
+                    .padding(.horizontal)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .tint(.black)
         }
         .padding()
     }
