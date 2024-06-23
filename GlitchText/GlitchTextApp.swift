b54e041efc9e088f7784134c8c32bbca06ab95a0
akbarshah19 <105868459+akbarshah19@users.noreply.github.com>
2024-06-23 11:47:03 +0500

    Initial Commit

---
+++ GlitchText/GlitchTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlitchTextApp.swift
+//  GlitchText
+//
+//  Created by Akbarshah Jumanazarov on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct GlitchTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
